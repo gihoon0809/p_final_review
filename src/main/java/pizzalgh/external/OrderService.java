3647c776f5ba6b3c853a80bf65904f8036c2ed66
gihoon0809 <gihoon0809@gmail.com>
2020-11-06 11:37:33 +0900

    test

--- src/main/java/pizzalgh/external/OrderService.java
+++ src/main/java/pizzalgh/external/OrderService.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-@FeignClient(name="order", url="http://order:8080")//origin
+//@FeignClient(name="order", url="http://order:8080")//origin
 //@FeignClient(name="order", url="http://localhost:8081")//local
-//@FeignClient(name="order", url="http://api.url.:8080")
+@FeignClient(name="order", url="http://api.url.order:8080")
 public interface OrderService {
 
     @RequestMapping(method= RequestMethod.POST, path="/orders")
